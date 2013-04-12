b59f9c2ccaf38f27cc52186ce4fe8556c7e88fac
Andreas Nylin <andreas.nylin@gmail.com>
2013-04-12 13:29:12 +0200

    Added v1.0 of ze code

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 jQuery.toggleAttr
 =================
 
-Plugin for toggling (the value of) a HTML element attribute
+Plugin for toggling (the value of) a HTML element attribute. This can be done with jQuery's *attr*, *removeAttr*, *prop* methods but I felt I wanted a cleaner way of doing this. The plugin is used by calling the *toggleAttr* method of a jQuery element. The method takes two arguments:
+
+**The first argument** is the name of the attribute. Calling the *toggleAttr* method with only one argument will simply add or remove the attribute from the element.
+
+**The second argument** is a single value or an array with several values for the attribute. Calling the *toggleAttr* method with two arguments where the second argument is a single value like a string or a number will add or remove the attribute for the HTML element. Calling the *toggleAttr* method with two arguments where the second argument is an array will cycle through the values in the array and update the attribute with the next value.
+
+Simple toggle attribute
+--------------
+$('#myElement').toggleAttr('disabled');
+$('#myElement').toggleAttr('disabled', 'disabled');
+
+Toggle two attribute values
+--------------
+$('#myElement').toggleAttr('aria-hidden', ['true', 'false']);
+
+Cycle several attribute values
+--------------
+$('#myElement').toggleAttr('value', ['A', 'B', 'C', 'D', 'E']);
---
+++ jquery.toggleattr.js
@@ -0,0 +1,51 @@
+/*
+ * jQuery Toggle Attribute Plugin - v1.0 - 2013-04-11
+ * Created by Andreas Nylin
+ * andreas.nylin@gmail.com / @andreasnylin / http://andreasnylin.com
+ */
+(function ($) {
+
+    $.fn.toggleAttr = function(attribute, attributeValues) {
+
+        function getNextValue(value) {
+            var i, l, nextValue;
+
+            for(i = 0, l = attributeValues.length; i < l; i++) {
+                if(attributeValues[i] === value) {
+                    break;
+                }
+            }
+
+            if(i + 1 >= attributeValues.length) {
+                nextValue = attributeValues[0];
+            } else {
+                nextValue = attributeValues[i + 1];
+            }
+
+            return nextValue;
+        }
+
+        return this.each(function () {
+            var $this = $(this),
+                hasValues = typeof attributeValues != 'undefined',
+                arrayValues = hasValues && attributeValues instanceof Array,
+                value = $this.attr(attribute),
+                newValue = null;
+
+            if(!hasValues && !$this.is('[' + attribute + ']')) {
+                $this.attr(attribute, attribute);
+            } else if(!hasValues) {
+                $this.removeAttr(attribute);
+            } else if(!arrayValues && value === attributeValues) {
+                $this.removeAttr(attribute);
+            } else if(!arrayValues) {
+                $this.attr(attribute, attributeValues);
+            } else if(arrayValues) {
+                $this.attr(attribute, getNextValue(value));
+            }
+
+        });
+
+    };
+
+})(jQuery);
